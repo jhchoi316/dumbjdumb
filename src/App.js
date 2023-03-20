7b0c5051cfc94058f14b36528fd408681529f440
[th_gus4734@g.hongik.ac.kr] <th_gus4734@g.hongik.ac.kr>
2023-03-20 13:39:05 +0900

    노드 연결 점 수정, 기본 노드 선 연결

--- src/App.js
+++ src/App.js
@@ -214,9 +214,25 @@ const DnDFlow = () => {
     },
 
   ]
+  const initialEdges = [
+    { id: '1-2', source: '1', target: '2' },
+    { id: '2-3', source: '2', target: '3' },
+    { id: '3-4', source: '3', target: '4' },
+    { id: '4-5', source: '4', target: '5' },
+    { id: '5-6', source: '5', target: '6' },
+    { id: '6-7', source: '6', target: '7' },
+    { id: '7-8', source: '7', target: '8' },
+    { id: '8-9', source: '8', target: '9' },
+    { id: '9-10', source: '9', target: '10' },
+    { id: '10-11', source: '10', target: '11' },
+    { id: '11-12', source: '11', target: '12' },
+    { id: '12-13', source: '12', target: '13' },
+    { id: '13-14', source: '13', target: '14' },
+
+  ];
   const reactFlowWrapper = useRef(null);
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
   const [reactFlowInstance, setReactFlowInstance] = useState(null);
 
   const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
